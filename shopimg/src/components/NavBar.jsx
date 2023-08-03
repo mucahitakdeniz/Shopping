ec3d033d6dff7b9997789b77cb9a6828b350cf27
Mücahit Akdeniz <mucahitakdeniz28@gmail.comgit config --global user.email mucahitakdeniz28@gmail.com>
2023-08-03 15:11:35 +0300

    navbar

--- shopimg/src/components/NavBar.jsx
+++ shopimg/src/components/NavBar.jsx
@@ -1,10 +1,23 @@
-
+import logo from "../assets/logo.png";
+import basket from "../assets/basket.png";
+import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
-    <div>
-      <img src="" alt="" />
-    </div>
-  )
-}
+    <div className="navbar">
+      <div className="navbarLeft">
+        <Link rel="stylesheet" to="/">
+          <img src={logo} className="logo" alt="logo" />{" "}
+        </Link>
 
-export default NavBar
+        <h2>Clarus Shopping</h2>
+      </div>
+      <div>
+        <Link rel="stylesheet" to="/basket">
+          <img src={basket} className="basketLogo" alt="basket" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
